4d76bc2d2331c9da27d82236317eb18ff109cc11
Jonatan Malm <jonatan.malm@live.se>
2020-12-10 15:50:54 +0100

    no longer using common js syntax

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,18 @@
-const express = require('express')
-const products = require('./data/products')
+import express from 'express'
+import products from './data/products.js'
+import env from 'dotenv'
+env.config()
 
 const app = express()
 
-app.listen(5000, console.log('Server running on port 5000'))
+const PORT = process.env.PORT || 5000
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}...`
+  )
+)
 
 app.get('/api/products', (req, res) => {
   res.json(products)
