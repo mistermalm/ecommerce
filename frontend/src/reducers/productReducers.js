21c6f41a965520f8c39eab9987ab0fc7939f3818
Jonatan Malm <jonatan.malm@live.se>
2020-12-19 19:24:24 +0100

    added single product page, constants, actions, reducers, view

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,9 +1,13 @@
 import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
-  PRODUCT_LIST_FAIL
+  PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL
 } from '../constants/productConstants'
 
+// reducer takes in two parameters         // initial state // action //
 export const productListReducer = (state = { products: [] }, action) => {
   switch (action.type) {
     case PRODUCT_LIST_REQUEST:
@@ -16,3 +20,22 @@ export const productListReducer = (state = { products: [] }, action) => {
       return state
   }
 }
+// reducer takes in two parameters // initial state // action //
+export const productDetailsReducer = (
+  // since a product contains a array called "reviews", i also set the initial state of "reviews" as an empty array
+  state = { product: { reviews: [] } },
+  action
+) => {
+  switch (action.type) {
+    case PRODUCT_DETAILS_REQUEST:
+      return { loading: true, ...state }
+    case PRODUCT_DETAILS_SUCCESS:
+      return { loading: false, product: action.payload }
+    case PRODUCT_DETAILS_FAIL:
+      return { loading: false, error: action.payload }
+    default:
+      return state
+  }
+}
+
+// IMPORT IN STORE
